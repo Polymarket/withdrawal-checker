a40bf7efea7bc30c0b1fd0e245adb549e86df6c8
cristiangpr <chivoblack@DESKTOP-0K5H7QO.localdomain>
2021-01-20 18:41:03 -0600

    wednesday

--- utils/utils.test.ts
+++ utils/utils.test.ts
@@ -19,7 +19,7 @@ describe("getLastBurnTxHash", () => {
 });
 
 describe("isBurnTxClaimable", () => {
-    test.only("should get the last burn tx for a given address", async function () {
+    test("should determine if withdrawal funds are available to be claimed for a given txHash", async function () {
         const burnTxHash =
             "0x0f7373fb7424b800b410646c71ef9bab95cc2b3c9440650e48de081fc5da0419";
         const result = await isBurnTxClaimable(
