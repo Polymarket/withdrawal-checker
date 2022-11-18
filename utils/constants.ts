40e51366643718f2f6a98730d95963c2e3ccab5a
dylangolow <dylangolow@gmail.com>
2022-11-18 16:05:22 -0500

    chore: upgrade next and react for security https://security.snyk.io/vuln/SNYK-JS-NEXT-1540422

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
-const MAINNET_NODE: string = "https://mainnet.infura.io/v3/a414a8f640db48c5aa8fcc3bf29353e8";
-const MATIC_NODE: string = "https://rpc-mainnet.maticvigil.com/v1/ea1bb94329e5fa87489704c1141745bdab51f1b0";
+const MAINNET_NODE: string = "https://eth-mainnet.alchemyapi.io/v2/3ZjURMaqw4EZ-2Oe3nafhnA7P-GgsYXR";
+const MATIC_NODE: string = "https://polygon-mainnet.g.alchemy.com/v2/WtlYIQrqH3Eao-TyQL7uFVPVILwQv7Yk";
 export const provider = new ethers.providers.JsonRpcProvider(MATIC_NODE);
 import UsdcAbi from "./abi/USDC.json";
 export const CONTRACT_ADDRESS: string = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
